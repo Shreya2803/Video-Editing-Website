b6587b766da85633ecdfda25fab0ce92085aadbb
SHREYA SOM <69584815+Shreya2803@users.noreply.github.com>
2022-03-07 03:18:43 +0530

    Update views.py

--- mysite/views.py
+++ mysite/views.py
@@ -61,6 +61,48 @@ def webpage2(request):
          
        
 def webpage3(request):
+      def webpage3(request):
+     if request.method == 'POST':
+         audio = request.POST.get('audio')
+         video = request.POST.get('video')
+         compress= request.POST.get('compress')
+         background_audio_clip = AudioFileClip(audio)
+         final_clip = video.set_audio(background_audio_clip)
+         final_clip.write_videofile(final_video_path, codec='libx264', audio_codec="aac")
+         if compress =='YES':
+             def compress_video( output_file_name, target_size):
+                  # Reference: https://en.wikipedia.org/wiki/Bit_rate#Encoding_bit_rate
+    min_audio_bitrate = 32000
+    max_audio_bitrate = 256000
+
+    probe = ffmpeg.probe(video_full_path)
+    # Video duration, in s.
+    duration = float(probe['format']['duration'])
+    # Audio bitrate, in bps.
+    audio_bitrate = float(next((s for s in probe['streams'] if s['codec_type'] == 'audio'), None)['bit_rate'])
+    # Target total bitrate, in bps.
+    target_total_bitrate = (target_size * 1024 * 8) / (1.073741824 * duration)
+
+    # Target audio bitrate, in bps
+    if 10 * audio_bitrate > target_total_bitrate:
+        audio_bitrate = target_total_bitrate / 10
+        if audio_bitrate < min_audio_bitrate < target_total_bitrate:
+            audio_bitrate = min_audio_bitrate
+        elif audio_bitrate > max_audio_bitrate:
+            audio_bitrate = max_audio_bitrate
+    # Target video bitrate, in bps.
+    video_bitrate = target_total_bitrate - audio_bitrate
+
+    i = ffmpeg.input(video_full_path)
+    ffmpeg.output(i, os.devnull,
+                  **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 1, 'f': 'mp4'}
+                  ).overwrite_output().run()
+    ffmpeg.output(i, output_file_name,
+                  **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 2, 'c:a': 'aac', 'b:a': audio_bitrate}
+                  ).overwrite_output().run()
+
+compress_video( 'output.mp4', 50 * 1000)
+   
   
       
     return render(request, 'page3.html')
